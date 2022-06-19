565064da18edf198c0661154372e26c5b064aac5
Sergey <komarovichs@gmail.com>
2022-06-19 15:46:32 +0600

    HTML, partials and SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,61 @@
-//gulp
+const {
+	src,
+	dest,
+	series,
+	watch
+  } = require('gulp');
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const del = require('del');
+const fileInclude = require('gulp-file-include');
+const sass = require('sass');
+const gulpSass = require('gulp-sass');
+const mainSass = gulpSass(sass);
+
+
+// Path
+const sourceFolder = './src';
+const buildFolder = './app';
+const path = {
+	srcpartialsHtml: `${sourceFolder}/partials`,
+	srcScss: `${sourceFolder}/scss/**/*.scss`,
+	buildCssFolder: `${buildFolder}/css`,
+}
+
+const clear = () => {
+	return del([buildFolder])
+}
+
+const htmlInclude = () => {
+	return src([`${sourceFolder}/*.html`])
+	.pipe(fileInclude({
+		prefix: '@',
+		basepath: '@file'
+	}))
+	.pipe(dest(buildFolder))
+	.pipe(browserSync.stream());
+}
+const styles = () => {
+	return src(path.srcScss)
+	.pipe(mainSass())
+	.pipe(dest(path.buildCssFolder))
+	.pipe(browserSync.stream());
+}
+
+const watchFiles = () => {
+	browserSync.init({
+		server: {
+		baseDir: `${buildFolder}`,
+		},
+		tunnel: 'true',
+		watch: true,
+	});
+	// watch(`${path.sourceFolder}/**`)
+	watch(`${path.srcpartialsHtml}/*.html`, htmlInclude);
+	watch(`${path.srcpartialsHtml}/**/*.html`, htmlInclude);
+	watch(`${sourceFolder}/*.html`, htmlInclude);
+	watch(path.srcScss, styles);
+}
+
+
+exports.default = series(clear, htmlInclude, styles, watchFiles);
