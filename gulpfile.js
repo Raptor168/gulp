76d06580e17d42845297540a939a30799a383f6a
Sergey <komarovichs@gmail.com>
2022-07-10 18:21:42 +0600

    update js, webp, avif

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,12 @@ const fileInclude = require('gulp-file-include');
 const sass = require('sass');
 const gulpSass = require('gulp-sass');
 const mainSass = gulpSass(sass);
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const webpackStream = require('webpack-stream');
 
 
 // Path
@@ -20,6 +26,12 @@ const path = {
 	srcpartialsHtml: `${sourceFolder}/partials`,
 	srcScss: `${sourceFolder}/scss/**/*.scss`,
 	buildCssFolder: `${buildFolder}/css`,
+	resFolder: `${sourceFolder}/resources`,
+	srcAllJs: `${sourceFolder}/js/**/*.js`,
+	srcMainJs: `${sourceFolder}/js/main.js`,
+	buildJsFolder: `${buildFolder}/js`,
+	srcImgFolder: `${sourceFolder}/img`,
+	buildImgFolder: `${buildFolder}/img`,
 }
 
 const clear = () => {
@@ -42,20 +54,78 @@ const styles = () => {
 	.pipe(browserSync.stream());
 }
 
+const resources = () => {
+	return src(`${path.resFolder}/**`)
+	.pipe(dest(buildFolder))
+}
+
+const scripts = () => {
+	return src(path.srcMainJs)
+	.pipe(plumber(
+		notify.onError({
+			title: "JS",
+			message: "Error: <%= error.message %>"
+		})
+	))
+	.pipe(webpackStream({
+		// mode: isProd ? 'production' : 'development',
+		mode: 'development',
+		output: {
+			filename: 'main.js',
+		},
+		module: {
+			rules: [{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				options: {
+					presets: [
+						['@babel/preset-env', {
+							targets: "defaults"
+							}]
+						]
+					}
+				}
+			}]
+		},
+		// devtool: !isProd ? 'source-map' : false
+	}))
+	.on('error', function (err) {
+		console.error('WEBPACK ERROR', err);
+		this.emit('end');
+	})
+	.pipe(dest(path.buildJsFolder))
+	.pipe(browserSync.stream());
+}
+
+const webpImages = () => {
+	return src([`${path.srcImgFolder}/**/**.{jpg,jpeg,png}`])
+	  .pipe(webp())
+	  .pipe(dest(path.buildImgFolder))
+  };
+  
+  const avifImages = () => {
+	return src([`${path.srcImgFolder}/**/**.{jpg,jpeg,png}`])
+	  .pipe(avif())
+	  .pipe(dest(path.buildImgFolder))
+  };
+
 const watchFiles = () => {
 	browserSync.init({
 		server: {
 		baseDir: `${buildFolder}`,
 		},
-		tunnel: 'true',
+		// tunnel: 'true',
 		watch: true,
 	});
-	// watch(`${path.sourceFolder}/**`)
 	watch(`${path.srcpartialsHtml}/*.html`, htmlInclude);
 	watch(`${path.srcpartialsHtml}/**/*.html`, htmlInclude);
 	watch(`${sourceFolder}/*.html`, htmlInclude);
-	watch(path.srcScss, styles);
+	watch(`${path.srcScss}`, styles);
+	watch(`${path.srcAllJs}`, scripts);
+	watch(`${path.resFolder}/**`, resources);
 }
 
 
-exports.default = series(clear, htmlInclude, styles, watchFiles);
+exports.default = series(clear, htmlInclude, styles, resources, scripts, webpImages, avifImages, watchFiles);
